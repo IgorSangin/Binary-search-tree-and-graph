aa695ad28bd809d10d1cdc0b0b53a891607cfb3a
Igor Sangin (sangini) <sangini@coventry.ac.uk>
2018-11-29 18:48:16 +0000

    Tidied code

--- ex4_is_connected.py
+++ ex4_is_connected.py
@@ -1,10 +1,18 @@
 class Graph(object):
+	"""Class for unweighted undirected graph
+	where key is a vertex
+	and values are neighbouring vertices"""
+	
 	def __init__(self):
+		"""Instantiation method,
+		initializes graph as a dictionary"""
+			
 		self.graph = {}
 		
 	def add_vertex(self,vertex):
 		"""Adding a vertex
 		as a key in dictionary"""
+		
 		if vertex not in self.graph:
 			self.graph[vertex] = []
 		else:
@@ -15,15 +23,23 @@ class Graph(object):
 		first vertex is key and second is value
 		and vice-versa
 		to connect vertices in a graph"""
+		
 		self.graph[firstVertex].append(secondVertex)
 		self.graph[secondVertex].append(firstVertex)
 			
 	def __repr__(self):
 		"""returns the graph"""
+		
 		return str(self.graph)
 								
 					
 	def isConnected(self):
+		"""Function to check if the graph is connected.
+		Graph is connected when there is a path between every pair of vertices.
+		Similar to depth first search algorithm,
+		travels the graph and if it is connected
+		prints Yes. Otherwise prints No."""
+		
 		stack = []
 		visited = []
 		start = list(self.graph.keys())[0] # start point of traversal
@@ -34,7 +50,7 @@ class Graph(object):
 				visited.append(popped)
 				for neighbour in self.graph[popped]:
 					stack.append(neighbour)
-		if len(visited) != len(self.graph.keys()):
+		if len(visited) != len(self.graph.keys()): # if number of visited keys not equal to number of total keys
 			print("No")
 		else:
 			print("Yes")
